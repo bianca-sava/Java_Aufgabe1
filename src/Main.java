ffe7c96ed6d7acbcaddb886320be4e2c10c785ba
Bianca Sava <biancasava07@gmail.com>
2025-10-15 17:24:07 +0300

    Added the examples in main Finished

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
+import java.util.List;
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello, World!");
-
-
+        GradeProcessor gradeProcessor = new GradeProcessor();
+        List<Integer> grades = List.of(29,37,38,41,84,67);
+        System.out.println("Failing grades: " + gradeProcessor.failingGrades(grades));
+        System.out.printf("Average grade: %.2f\n", gradeProcessor.averageGrade(grades));
+        System.out.println("Rounded grades: " + gradeProcessor.roundedGrades(grades));
+        System.out.println("Max grade: " + gradeProcessor.maxGrade(grades));
 
     }
 }
